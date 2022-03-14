01c0917e4d04321b7484ad2f323db28fe739e980
valtok7 <morisoba2013@gmail.com>
2022-03-15 04:46:26 +0900

    tidy

--- DoClangTidy.cmake
+++ DoClangTidy.cmake
@@ -1,17 +1,11 @@
 cmake_minimum_required(VERSION 3.6)
-option(CLANG_TIDY_ENABLE
-  "If the command clang-tidy is avilable, tidy source files.\
-Turn this off if the build time is too slow."
-  ON)
-find_program(CLANG_TIDY_EXE clang-tidy)
 
-function(clang_tidy target)
-  if(CLANG_TIDY_EXE)
-    if(CLANG_TIDY_ENABLE)
-      message(STATUS "Enable Clang-Tidy ${target}")
-      set_target_properties(${target} PROPERTIES
-        C_CLANG_TIDY "${CLANG_TIDY_EXE};-fix;-fix-errors"
-        CXX_CLANG_TIDY "${CLANG_TIDY_EXE};-fix;-fix-errors")
-      endif()
-  endif()
-endfunction()
+find_program(CLANG_TIDY_EXE NAMES "clang-tidy" DOC "Path to clang-tidy executable" )
+if(NOT CLANG_TIDY_EXE) 
+  message(STATUS "clang-tidy not found.") 
+else() 
+  message(STATUS "clang-tidy found: ${CLANG_TIDY_EXE}") 
+#  set(DO_CLANG_TIDY "${CLANG_TIDY_EXE}" "-checks=*,-clang-analyzer-alpha.*") 
+  set(DO_CLANG_TIDY "${CLANG_TIDY_EXE}" 
+    "-checks=*,bugprone-macro-parentheses,bugprone-redundant-branch-condition,cppcoreguidelines-init-variables,cppcoreguidelines-pro-type-member-init,misc-redundant-expression,cppcoreguidelines-pro-type-cstyle-cast,cppcoreguidelines-virtual-class-destructor,llvm-include-order,misc-definitions-in-headers,modernize-redundant-void-arg,modernize-use-bool-literals,portability-restrict-system-includes,readability-convert-member-functions-to-static,readability-make-member-function-const, readability-non-const-parameter,readability-qualified-auto,readability-redundant-control-flow,readability-redundant-member-init,readability-const-return-type,misc-static-assert,modernize-make-shared,modernize-make-unique,modernize-use-auto,modernize-use-default-member-init,modernize-use-emplace,modernize-use-equals-delete,modernize-use-nullptr,modernize-use-override,modernize-use-using")
+endif()
